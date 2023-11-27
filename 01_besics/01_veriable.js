486614a68c9caf9f71986d8c91d283182bd5df0b
angikbiswas <136958221+angikbiswas@users.noreply.github.com>
2023-11-27 18:34:22 +0000

    Day one javaScript done

---
+++ 01_besics/01_veriable.js
@@ -0,0 +1,13 @@
+const accountID= 45678
+let accountName= 'Angik'
+var accountPassword= '8907'
+accountcity= 'kolkata'
+
+//accountID=3
+
+accountName= 'Tulan'
+accountPassword='1999'
+accountcity= 'hyderabad'
+
+console.log(accountID)
+console.table([accountID, accountName,accountPassword,accountcity])
